48499fa4036edb0958f0c15347295ff850846e6d
Tarcísio Couto <tarcisio.pereira@caruaru.ifpe.edu.br>
2019-10-16 14:36:25 -0300

    melhoria no textscreen

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -3,19 +3,21 @@ import { Text, View, StyleSheet, TextInput } from 'react-native';
 
 const TextScreen = () => {
 
-    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
 
     return (
         <View>
-            <Text>Enter Name:</Text>
+            <Text>Enter Password:</Text>
             <TextInput
                 style={styles.input}
                 autoCapitalize = 'none'
                 autoCorrect={false}
-                value={name}
-                onChangeText={(newValue) => setName(newValue)}
+                value={password}
+                onChangeText={(newValue) => {
+                    setPassword(newValue)
+                }}
             />
-            <Text>My Name is: {name}</Text>
+            { password.length <= 5 ? <Text>Password must be longer than 5 characters</Text> : null}
         </View>
     );
 };
